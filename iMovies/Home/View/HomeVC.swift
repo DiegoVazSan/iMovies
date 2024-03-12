b23864b91d26b0c5c72209cb60dba625b66ddedc
Diego Vazquez Sanchez <diegov@Diegos-MacBook-Pro.local>
2024-03-12 13:46:01 -0600

    FEAT: Episode 6 - RxDataSources

--- iMovies/Home/View/HomeVC.swift
+++ iMovies/Home/View/HomeVC.swift
@@ -20,27 +20,31 @@ class HomeVC: UIViewController {
     private var router = HomeRouter()
     private var viewModel = HomeViewModel()
     
-    let movies : BehaviorRelay<[Movie]> = BehaviorRelay.init(value: [
-        Movie.init(name: "Avatar, the way of water", img: "avatar"),
-        Movie.init(name: "The Batman", img: "batman"),
-        Movie.init(name: "Forest Gump", img: "forest"),
-        Movie.init(name: "Gladiator", img: "gladiator"),
-        Movie.init(name: "Pulp Fiction by Quentin Tarantino", img: "pulpF"),
-        Movie.init(name: "Terminator", img: "terminator"),
-        Movie.init(name: "Rambo", img: "Rambo"),
-        Movie.init(name: "Thor", img: "thor"),
-        Movie.init(name: "Capitan America", img: "ca"),
-        Movie.init(name: "Spiderman", img: "spider"),
-        Movie.init(name: "Guardians Of The Galaxy", img: "guardians"),
-        Movie.init(name: "dardevil", img: "Daredevil"),
-        Movie.init(name: "Punisher", img: "punisher")
+    
+    let tblViewItemsSectioned = BehaviorRelay.init(value: [
+        SectionModel(header: "Disney Movies", items: [Movie.init(name: "Avatar, the way of water", img: "avatar"),
+                                                      Movie.init(name: "Thor", img: "thor"),
+                                                      Movie.init(name: "Capitan America", img: "ca"),
+                                                      Movie.init(name: "Spiderman", img: "spider"),
+                                                      Movie.init(name: "Guardians Of The Galaxy", img: "guardians"),
+                                                      Movie.init(name: "dardevil", img: "Daredevil"),
+                                                      Movie.init(name: "Punisher", img: "punisher")]),
+        SectionModel(header: "Adventure", items: [Movie.init(name: "The Batman", img: "batman"),
+                                                  Movie.init(name: "Forest Gump", img: "forest"),
+                                                  Movie.init(name: "Gladiator", img: "gladiator"),
+                                                  Movie.init(name: "Pulp Fiction by Quentin Tarantino", img: "pulpF"),
+                                                  Movie.init(name: "Terminator", img: "terminator"),
+                                                  Movie.init(name: "Rambo", img: "Rambo")])
     ])
+    
     let disposeBag = DisposeBag()
     
     let dataSource = RxTableViewSectionedReloadDataSource<SectionModel>(configureCell: {
         ds, tv, ip, item in
         let cell : MainMovieCell = tv.dequeueReusableCell(withIdentifier: MainMovieCell.keyID, for: ip) as! MainMovieCell
         cell.movieTitle.text = item.name
+        cell.movieImgView.image = UIImage(named: item.img)
+        cell.selectionStyle = .none
         return cell
     },
        titleForHeaderInSection: {
@@ -65,19 +69,19 @@ class HomeVC: UIViewController {
             .distinctUntilChanged()///Aseguramos que se emitan eventos SI la consulta a cambiado al ulitmo valor emitido
             .map({
                 query in
-                self.movies.value.filter({
-                    movie in
+                
+                self.tblViewItemsSectioned.value.map({
+                    sectionModel in
+                    SectionModel(header: sectionModel.header, items: sectionModel.items.filter({
+                        movie in
+                        query.isEmpty || movie.name.lowercased().contains(query.lowercased())
+                    }))
                     
-                    ///Determinamos si la consulta esta vacia o confirmamos si la consulta tiene coincidencias con algun elemento de movies
-                    query.isEmpty || movie.name.lowercased().contains(query.lowercased())
-                    
-                }) ///Vinculamos los resultados de la busqueda a la table view
-            }).bind(to: tblView.rx.items(cellIdentifier: MainMovieCell.keyID, cellType: MainMovieCell.self)){ (row, movie, cell) in
-                print("La pelicula que conicide con las busqeudas es: \(movie.name)")
-                cell.movieTitle.text = movie.name
-                cell.movieImgView.image = UIImage.init(named: movie.img)
-                cell.selectionStyle = .none
-            }
+                })
+                ///Vinculamos los resultados de la busqueda a la table view
+            }).bind(to: tblView
+                .rx
+                .items(dataSource: dataSource))
             .disposed(by: disposeBag)
         
         tblView.rx.modelSelected(Movie.self).subscribe(onNext: {
